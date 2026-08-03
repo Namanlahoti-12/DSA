bd827cd7940719e5294e38dad7c7e840a5a0c352
Naman Lahoti <namanlahoti1024@gmail.com>
2026-08-04 00:04:40 +0530

    Add files via upload

---
+++ DSA LAB ASSIGNMENT/Parenthesis.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <string.h>
+struct stack{
+char arr[20];
+int top;
+};
+struct stack s;
+int isempty(){
+    if(s.top==-1){
+        return 1;}
+    else
+        return 0;
+    }
+int isfull(){
+    if(s.top==9){
+        return 1;}
+    else
+        return 0;
+    }
+
+
+int push(char a){
+    if(isfull()==1){
+        printf("Stack is full!!");
+    }
+    else{
+        s.top++;
+        s.arr[s.top]=a;
+    }
+    }
+int pop(){
+    if(isempty()==1){
+        printf("Stack is empty!!");
+    }
+    else{
+        s.top--;
+    }}
+int main(){
+    s.top=-1;
+    int flag;
+    char str[20];
+    printf("Enter String: ");
+    scanf("%s",str);
+    size_t x=strlen(str);
+    for(int i=0;i<x;i++){
+        if(str[i]=='(' ||str[i]=='{'||str[i]=='['){
+            push(str[i]);}
+        else{
+            char z=str[i];
+                if((z==')') && (s.arr[s.top]=='('))
+                    flag=1;
+                else if((z==']') && (s.arr[s.top]=='['))
+                    flag=1;
+                else if((z=='}') && (s.arr[s.top]=='{')){
+                    flag=1;}
+                else
+                    flag=0;
+                pop();
+                }}
+        if(flag==1)
+            printf("Balanced");
+        else
+            printf("Unbalanced");
+
+return 0;}
